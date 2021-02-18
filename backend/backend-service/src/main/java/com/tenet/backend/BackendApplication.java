0b60aa787694bd8b6291207067870f195bb71ef6
Madison <jianrong@datastory.com.cn>
2021-02-18 10:27:23 +0800

    commit

---
+++ backend/backend-service/src/main/java/com/tenet/backend/BackendApplication.java
@@ -0,0 +1,17 @@
+package com.tenet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Madison
+ * @since  2021/2/5
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class);
+    }
+
+}
--- backend/backend-service/src/main/java/com/tenet/backend/controller/BcMenuController.java
+++ backend/backend-service/src/main/java/com/tenet/backend/controller/BcMenuController.java
@@ -1,8 +1,14 @@
 package com.tenet.backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
+ * 菜单管理
+ *
  * @author MuggleStar
  * @date 2021/2/5 0:37
  */
+@RestController
 public class BcMenuController {
 }

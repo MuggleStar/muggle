d81277569fba36dfdb428d0ff46a47c86d181bbd
Madison <MuggleStar@163.com>
2021-03-29 02:51:14 +0800

    commit

--- script/src/main/resource/elasticsearch/暂存.sql
+++ script/src/main/resource/elasticsearch/暂存.sql
@@ -9,91 +9,6 @@ POST /_aliases
 }
 
 
-GET goods_search/_search
-{
-  "query": {
-    "bool": {
-      "filter": [
-        {
-          "nested": {
-            "path": "genericSpec",
-            "query": {
-              "bool": {
-                "must": [
-                  {
-                    "bool": {
-                      "must": [
-                        {
-                          "term": {
-                            "genericSpec.key": "1"
-                          }
-                        },
-                        {
-                          "term": {
-                            "genericSpec.value": "华为（HUAWEI）"
-                          }
-                        }
-                      ]
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        },
-        {
-          "nested": {
-            "path": "specialSpec",
-            "query": {
-              "bool": {
-                "must": [
-                  {
-                    "bool": {
-                      "must": [
-                        {
-                          "term": {
-                            "specialSpec.key": "4"
-                          }
-                        }
-                      ],
-                      "should": [
-                        {
-                          "term": {
-                            "specialSpec.value": "幻夜黑"
-                          }
-                        },
-                        {
-                          "term": {
-                            "specialSpec.value": "极光蓝"
-                          }
-                        }
-                      ],"minimum_should_match": 1
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        }
-      ],
-      "should": [
-        {"term": {
-          "all": "手机"
-          }
-        },{
-          "term": {
-            "goCategoryIdThree":"76"
-          }
-        }
-      ],"minimum_should_match": 1
-    }
-  },
-  "rescore": {
-    "query": {},
-    "window_size": 50
-  }
-}
-
 
 ## 2021-03-26 查询+品牌聚合+分类聚合
 GET goods_search_alias/_search
@@ -103,5 +18,13 @@ GET goods_search_alias/_search
 GET goods_search_alias/_search
 {"query":{"bool":{"filter":[{"nested":{"path":"genericSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"genericSpec.key":"7"}},{"term":{"genericSpec.value":"Android"}}]}}]}}}},{"nested":{"path":"specialSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"specialSpec.key":"4"}},{"terms":{"specialSpec.value":["幻夜黑","极光蓝","黑色"]}}]}}]}}}}],"must":[{"match":{"all":"手机 华为 小米"}},{"term":{"goCategoryIdOne":"74"}}]}},"aggs":{"goBrandId":{"terms":{"field":"goBrandId"}},"goCategoryIdThree":{"terms":{"field":"goCategoryIdThree"}},"genericSpec":{"nested":{"path":"genericSpec"},"aggs":{"genericSpec.key":{"terms":{"field":"genericSpec.key"},"aggs":{"genericSpec.value":{"terms":{"field":"genericSpec.value"}}}}}},"specialSpec":{"nested":{"path":"specialSpec"},"aggs":{"specialSpec.key":{"terms":{"field":"specialSpec.key"},"aggs":{"specialSpec.value":{"terms":{"field":"specialSpec.value"}}}}}}}}
 
+## 2021-03-26 查询+品牌聚合+分类聚合+参数聚合+排序+min_score
+GET goods_search_alias/_search
+{"min_score":3,"query":{"bool":{"filter":[{"nested":{"path":"genericSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"genericSpec.key":"7"}},{"term":{"genericSpec.value":"Android"}}]}}]}}}},{"nested":{"path":"specialSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"specialSpec.key":"4"}},{"terms":{"specialSpec.value":["幻夜黑","极光蓝","黑色"]}}]}}]}}}}],"must":[{"match":{"all":"手机华为小米"}},{"term":{"goCategoryIdOne":"74"}}]}},"aggs":{"goBrandId":{"terms":{"field":"goBrandId"}},"goCategoryIdThree":{"terms":{"field":"goCategoryIdThree"}},"genericSpec":{"nested":{"path":"genericSpec"},"aggs":{"genericSpec.key":{"terms":{"field":"genericSpec.key"},"aggs":{"genericSpec.value":{"terms":{"field":"genericSpec.value"}}}}}},"specialSpec":{"nested":{"path":"specialSpec"},"aggs":{"specialSpec.key":{"terms":{"field":"specialSpec.key"},"aggs":{"specialSpec.value":{"terms":{"field":"specialSpec.value"}}}}}}},"sort":[{"priceList":{"order":"desc"}}]}
+
+
+## 2021-03-26 查询+品牌聚合+分类聚合+参数聚合+rescore (用了rescore 就不能用 sort 慎用)
+GET goods_search_alias/_search
+{"query":{"bool":{"filter":[{"nested":{"path":"genericSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"genericSpec.key":"7"}},{"term":{"genericSpec.value":"Android"}}]}}]}}}},{"nested":{"path":"specialSpec","query":{"bool":{"must":[{"bool":{"must":[{"term":{"specialSpec.key":"4"}},{"terms":{"specialSpec.value":["幻夜黑","极光蓝","黑色"]}}]}}]}}}}],"must":[{"match":{"all":"手机华为小米"}},{"term":{"goCategoryIdOne":"74"}}]}},"aggs":{"goBrandId":{"terms":{"field":"goBrandId"}},"goCategoryIdThree":{"terms":{"field":"goCategoryIdThree"}},"genericSpec":{"nested":{"path":"genericSpec"},"aggs":{"genericSpec.key":{"terms":{"field":"genericSpec.key"},"aggs":{"genericSpec.value":{"terms":{"field":"genericSpec.value"}}}}}},"specialSpec":{"nested":{"path":"specialSpec"},"aggs":{"specialSpec.key":{"terms":{"field":"specialSpec.key"},"aggs":{"specialSpec.value":{"terms":{"field":"specialSpec.value"}}}}}}},"rescore":{"window_size":1000,"query":{"score_mode":"multiply","rescore_query":{"function_score":{"script_score":{"script":{"source":"1/(Math.log10(doc.priceList.value+2))"}}}}}}}
 
 
--- search/search-service/src/test/java/com/teste/EsTest.java
+++ search/search-service/src/test/java/com/teste/EsTest.java
@@ -13,13 +13,32 @@ import com.tenet.search.entity.GoodsSearch;
 import com.tenet.search.repository.GoodsSearchRepository;
 import com.tenet.search.service.GoodSyncService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.nested.NestedAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +56,9 @@ public class EsTest {
 
     @Resource
     private GoodsSearchRepository goodsSearchRepository;
+
+    @Resource
+    private RestHighLevelClient restHighLevelClient;
 
     @Resource
     private GoodSyncService goodSyncService;
@@ -58,5 +80,52 @@ public class EsTest {
     public void testInitGoods() {
         goodSyncService.initGoods();
     }
+
+    @Test
+    public void testSearch() {
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        searchSourceBuilder
+                .minScore(3)
+                .query(QueryBuilders.boolQuery()
+                        .filter(QueryBuilders.nestedQuery("genericSpec",
+                                QueryBuilders.boolQuery().must(
+                                        QueryBuilders.boolQuery()
+                                                .must(QueryBuilders.termQuery("genericSpec.key","7"))
+                                                .must(QueryBuilders.termQuery("genericSpec.value","Android"))),
+                                ScoreMode.None))
+                        .filter(QueryBuilders.nestedQuery("specialSpec",
+                                QueryBuilders.boolQuery().must(
+                                        QueryBuilders.boolQuery()
+                                                .must(QueryBuilders.termQuery("specialSpec.key","4"))
+                                                .must(QueryBuilders.termsQuery("specialSpec.value","幻夜黑", "极光蓝", "黑色"))),
+                                ScoreMode.None))
+                        .must(QueryBuilders.matchQuery("all","手机华为小米"))
+                        .must(QueryBuilders.termQuery("goCategoryIdOne","74")))
+                .aggregation(AggregationBuilders.terms("goBrandId").field("goBrandId"))
+                .aggregation(AggregationBuilders.terms("goCategoryIdThree").field("goCategoryIdThree"))
+                .aggregation(AggregationBuilders.nested("genericSpec", "genericSpec")
+                        .subAggregation(AggregationBuilders.terms("genericSpec.key")
+                                .field("genericSpec.key")
+                                .subAggregation(AggregationBuilders.terms("genericSpec.value")
+                                        .field("genericSpec.value"))))
+                .sort(SortBuilders.fieldSort("priceList").order(SortOrder.DESC))
+                .from(1).size(10);
+
+
+        SearchRequest searchRequest = new SearchRequest("goods_search_alias");
+
+        System.out.println(searchSourceBuilder.toString());
+
+        try {
+            SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+            System.out.println(search);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
